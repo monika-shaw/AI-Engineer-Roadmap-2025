b8fb3f7673da04e157e71856e0d2678031dd685f
shawmonika616ms <shawmonika616ms@gmail.com>
2025-10-30 14:04:49 +0530

    Cleaned data and added column

--- Day 2/netflix_dashboard/src/dashboard.py
+++ Day 2/netflix_dashboard/src/dashboard.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-from data_processing import load_data
+from data_processing import load_data, clean_data, feature_engineering
 
 def main():
     st.title("Netflix Titles Dashboard")
@@ -10,6 +10,8 @@ def main():
     data_path = os.path.join(base_dir, '..', 'data', 'netflix_titles.csv')
 
     df = load_data(data_path)
+    df = clean_data(df)
+    df = feature_engineering(df)
 
     st.subheader("Raw Data Sample")
     st.dataframe(df.head(10))
--- Day 2/netflix_dashboard/src/data_processing.py
+++ Day 2/netflix_dashboard/src/data_processing.py
@@ -4,3 +4,18 @@ import numpy as np
 def load_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     return df
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop_duplicates()
+
+    df['country'] = df['country'].fillna('Unknown')
+    df['rating'] = df['rating'].fillna('Not Rated')
+
+    if 'date_added' in df.columns:
+        df['date_added'] = pd.to_datetime(df['date_added'].str.strip(), errors='coerce')
+    return df
+
+def feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
+    if 'date_added' in df.columns:
+        df['year_added'] = df['date_added'].dt.year
+    return df
